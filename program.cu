bba8c477b8a98dbc0cef7404b47560df69950d4a
Sergio Carlos Morales <carloschilazo@gmail.com>
2012-01-29 22:05:24 -0600

    Fixed index calculation problems

--- program.cu
+++ program.cu
@@ -4,8 +4,7 @@
 #include <cuda.h>
 using namespace std;
 
-
-//cannot use builtin functions on the device, we have to manually define our own, therefore I have to reinvent the wheel here
+/*  Cannot use built-in functions, need to rewrite this so it can be run on the device, kinda reinventing the wheel over here */
 __device__ void calculate_exponent(int base,int exponent,long &result){
 	result = 1L;
 	if(exponent==0){
@@ -19,14 +18,14 @@ __device__ void calculate_exponent(int base,int exponent,long &result){
 
 __global__ void evaluate(int *input, int totalSizeOfArray, int number_genes, int individualsPerThread, int number_blocks, int threads_per_block, long *scores){
 
-	/*gloabl position in population array index calculation */
-	int startingPosition = (blockIdx.y * threads_per_block * number_genes) + (threadIdx.y * number_genes);
+	/*global position in population array index calculation */
+	int startingPosition = (blockIdx.y * threads_per_block * number_genes * individualsPerThread) + (threadIdx.y * number_genes * individualsPerThread);
 	if(startingPosition>=totalSizeOfArray){
-		//return; //*return if thread is useless */
+		return; //*return if thread is useless */
 	}
 
-	/*gloabl position in scores array index calculation  REDO*/
-	int startingPosition_scores = (blockIdx.y * threads_per_block) + threadIdx.y;
+	/*global position in scores array index calculation  */
+	int startingPosition_scores = (blockIdx.y * threads_per_block * individualsPerThread) + (threadIdx.y * individualsPerThread);
 
 
 	long acumulated = 0L;
@@ -50,15 +49,15 @@ __global__ void evaluate(int *input, int totalSizeOfArray, int number_genes, int
 
 
 	int main(){
-		//define settings
+		/* define settings */
 		const unsigned int number_genes = 10;
-		const unsigned int number_individuals = 101;
+		const unsigned int number_individuals = 10000000;
 
-		const unsigned int threads_per_block_evaluation = 10; //DO NOT FORGET: BLOCK IS 1 thread width, and threads_per_block  height
-		const unsigned int individuals_per_thread_evaluation = 3;
+		const unsigned int threads_per_block_evaluation = 500; //DO NOT FORGET: BLOCK IS 1 thread width, and threads_per_block  height, MAX 512
+		const unsigned int individuals_per_thread_evaluation = 50;
 
 
-		//allocate and randomly initialize memory for population
+		/* allocate and randomly initialize memory for population */
 		int *population_array_host = new int[number_genes*number_individuals];
 		int *population_array_device;
 		srand ( time(NULL) );
@@ -68,7 +67,7 @@ __global__ void evaluate(int *input, int totalSizeOfArray, int number_genes, int
 		size_t memory_for_population = number_genes*number_individuals*sizeof(int);
 		cudaMalloc((void **) &population_array_device, memory_for_population);
 
-		//allocate and zeroise scores array, avoid any future issues with non initialized arrays
+		/* allocate and zeroise scores array, avoid any future issues with non initialized arrays */
 		long *scores_array_host = new long[number_individuals];
 		long *scores_array_device;
 		for(int contador=0;contador<number_individuals;contador++){
@@ -77,41 +76,26 @@ __global__ void evaluate(int *input, int totalSizeOfArray, int number_genes, int
 		size_t memory_for_scores = number_individuals*sizeof(long);
 		cudaMalloc((void **) &scores_array_device, memory_for_scores);
 
-		//we move data from host to device
+		/* we move data from host to device */
 		cudaMemcpy(population_array_device, population_array_host, memory_for_population, cudaMemcpyHostToDevice);
 		cudaMemcpy(scores_array_device, scores_array_host, memory_for_scores, cudaMemcpyHostToDevice);
 
-		//we calculate dimensions for grid and blocks and create them: for evaluation
+		/* we calculate dimensions for grid and blocks and create them: for evaluation */
 		unsigned int blocks_required_evaluation = number_individuals/(threads_per_block_evaluation *individuals_per_thread_evaluation) + 
 																	 (number_individuals%(threads_per_block_evaluation *individuals_per_thread_evaluation) == 0 ? 0:1);
 
-		dim3 grid_evaluation(1,blocks_required_evaluation); //in terms of blocks
-		dim3 block_evaluation(1,threads_per_block_evaluation); // in terms of threads
+		dim3 grid_evaluation(1,blocks_required_evaluation); /* in terms of blocks */
+		dim3 block_evaluation(1,threads_per_block_evaluation); /* in terms of threads*/
 
 		cout << "Blocks " << blocks_required_evaluation<< endl;
 		cout << "Threads " << threads_per_block_evaluation<< endl;
-		
-		//we launch evaluation kernel: evaluate(int *input, int totalSizeOfArray, int number_genes, int individualsPerThread, int number_blocks, int threads_per_block, long *scores)
+		cout << "Start";
+		/*we launch evaluation kernel: evaluate(int *input, int totalSizeOfArray, int number_genes, int individualsPerThread, int number_blocks, int threads_per_block, long *scores)*/
 		evaluate <<< grid_evaluation, block_evaluation >>> (population_array_device, number_genes*number_individuals, number_genes, individuals_per_thread_evaluation, blocks_required_evaluation, threads_per_block_evaluation, scores_array_device);
 		
-		cudaMemcpy(scores_array_host, scores_array_device, memory_for_scores, cudaMemcpyDeviceToHost);
-		cudaMemcpy(population_array_host, population_array_device, memory_for_population, cudaMemcpyDeviceToHost);
+		/*cudaMemcpy(scores_array_host, scores_array_device, memory_for_scores, cudaMemcpyDeviceToHost);*/
+		/*cudaMemcpy(population_array_host, population_array_device, memory_for_population, cudaMemcpyDeviceToHost);*/
 
-
-	for(int contador=0;contador<number_genes*number_individuals;contador++){
-		if(contador%number_genes==0 && contador > 0){
-			//cout << endl;
-		}
-		//cout << population_array_host[contador] << "-";
-
-	}
-	cout << endl;
-	cout << "----";
-	cout << endl;
-	
-	for(int contador=0;contador<number_individuals;contador++){
-		cout << scores_array_host[contador] << endl;
-	}
 
 		return 0;
 	}
